2bea34f870341c8244693f044cfb63f005bccdb5
Michael Cangcianno <michaelcangcianno@Michaels-MacBook-Pro.local>
2018-04-09 19:01:58 +0700

    edit hitungHuruf.js

---
+++ week4/hitungHuruf.js
@@ -0,0 +1,16 @@
+function hitungHuruf(kata) {
+  var terbanyak = '';
+  var jumlahHuruf = 0;
+  var arr = kata.split(' ');
+  for (var i = 0; i < arr.length; i++) {
+    
+  }
+  return terpanjang;
+}
+
+// TEST CASES
+console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
+console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+console.log(hitungHuruf('mengayuh perahu di danau')); // danau
